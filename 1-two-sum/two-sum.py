6294bd069632db59324427b7779147781adb038a
Norbert Shema <shemanorbert11@gmail.com>
2025-06-27 16:50:39 -0400

    Update two-sum.py

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -9,6 +9,6 @@ class Solution(object):
         for i, num in enumerate(nums):
             if target-num in lookup:
                 return [lookup[target - num], i]
-        lookup[num] = i
+            lookup[num] = i
         return []
         
