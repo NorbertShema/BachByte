98fbbbd4f048e7558511e907b5fe94964b015c6c
Norbert Shema <shemanorbert11@gmail.com>
2025-07-09 22:14:33 -0400

    Update reverse-linked-list.py

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -10,8 +10,9 @@ class Solution(object):
         :rtype: Optional[ListNode]
 
         Hint:
-            -The prev pointer is going to be the new head afte the linked listed have been reversed.
-            -
+            -The prev pointer is going to be the new head after the linked listed has been reversed.
+        
+            - follow the order( initialize pointer, flip them to reversed and keep moving till the end of the linked list)
 
 
         """
@@ -19,9 +20,9 @@ class Solution(object):
         prev = None
 
         while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+            temp = curr.next  ## temp pointer 
+            curr.next = prev  ## switches the nodes and keeps moving till the end
+            prev = curr        ## switches the pointer
+            curr = temp         ## switches the pointer
 
         return prev    
