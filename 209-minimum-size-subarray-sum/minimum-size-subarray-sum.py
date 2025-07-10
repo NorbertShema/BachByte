1cbbc338454065498406ab3c17c16f91cb430545
Norbert Shema <shemanorbert11@gmail.com>
2025-07-10 16:06:20 -0400

    Time: 12 ms (73.75%) | Memory: 20.2 MB (36.52%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,20 +5,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
 
+        understand
+            -Output :int
+            -Input: int
+            -Constraints: time and space complexity
+            -Edge cases: empty array
         plan
             - Use sliding windows to track the size of subarray
+
         """
-        min_length = float('inf')
-        summ = 0
-        l = 0 ## length
+        min_length = float('inf') ## initialize the min length to infinity
+        summ = 0                  ## initialize the sum to keep track of the current window sum
+        left = 0                     ## initialize the left pointer
 
-        for r in range (len(nums)):
-            summ += nums [r]
-
-            while summ >= target:
-                min_length = min(min_length, r-l+1)
-                summ -= nums [l]
-                l +=1
+        for right in range (len(nums)):
+            summ += nums [right]
+            ## while we have a valid window, meaning sum is great/equal to the target
+            while summ >= target:  
+            ## compares the min length and the current lenght and returns the min between them.
+                min_length = min(min_length, right-left+1) 
+                ## adjust the sum to decrease the window by subracting l from the window,
+                summ -= nums[left]  
+                left +=1           ## move the pointer down
 
         return min_length if min_length < float('inf') else 0        
 
