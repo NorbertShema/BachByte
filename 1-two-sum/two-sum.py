a0d67824435e2155afd32dce15d0f3c1f08a4efa
Norbert Shema <shemanorbert11@gmail.com>
2025-07-12 21:47:50 -0400

    Time: 0 ms (100.00%) | Memory: 12.9 MB (90.14%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,10 +4,37 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
+
+        Understand:
+
+            -Input: list (int) and a target (int)
+            -Output: list of indices
+            -Constraint: 
+                        -only one solution 
+                        - not using the same element twice
+            -Edge cases: empty array  
+
+
+         Plan:
+                -Use a hash map to store key:value pairs as (num:index)      
+
+
+        implement:
+                       
         """
+
         lookup = {}
-        for i, num in enumerate(nums):
-            if target - num in lookup:
-                return [lookup[target - num], i]
-            lookup[num] = i
-        return []
+
+        for i, num in enumerate(nums):       # Iterate through the list with index and value
+            x = target - num                 # two numbers that adds up to the target ( we know num and we know the target)
+
+            if x in lookup:                   # Check if the complement is already in the dictionary
+                return [lookup[x], i]         # If found, return the indices of the two numbers
+                
+            lookup[num] = i                   # Otherwise, store the current number with its index in the dictionary
+
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
