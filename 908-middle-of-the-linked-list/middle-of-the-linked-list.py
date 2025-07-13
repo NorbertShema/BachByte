61cbb19c217eedc4ad84705f973a48764b6ae45e
Norbert Shema <shemanorbert11@gmail.com>
2025-07-12 20:57:37 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.56%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -3,19 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution(object):
     def middleNode(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
+
+        Understand
+            -input: linked list 
+            -output: middle node
+            -Constraints: The list is non-empty and then node values are positive integers
+            =Edge cases:-Even number of nodes, return the second mid. 
+                        - Odd number of nodes, return the middle node.
+
+        plan:
+            - Use fast and slow pointer: This will work because the fast pointer moves twice as fast as the slow pointer.
+             so when the fast gets at the end of the linked list, the slow will be exactly pointing at the middle node.
+
+        Implement
+            
         """
-        slow = fast = head
+        fast = head                     ##initialize the pointers
+        slow = head
 
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            fast = fast.next.next       ## fast moves two times
+            slow = slow.next            ## slow moves once
 
-        return slow
-
-        
+        return slow        
