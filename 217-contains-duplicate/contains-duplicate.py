309abf4ae7e5a2d9caee0a8eabcbe089e1706878
Norbert Shema <shemanorbert11@gmail.com>
2025-07-16 14:02:26 -0400

    Time: 21 ms (39.94%) | Memory: 26 MB (17.99%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+
+        Understand:
+                -Input: list (int)
+                -Output: bool (true/false)
+                -Constraints: time/space complexity, 
+                -Edge cases: empty list
+
+        plan:
+            -Use set to avoid duplicates  
+            -Make an iteration to check if the num is in the set, 
+            if not add it. other keep moving until we reach the end of the list
+            -if the list is empty , then return true since we have no dups
+
+
+        Implement:
+          
+
+        """
+        seen = set ()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False      
+
+
+
+        
