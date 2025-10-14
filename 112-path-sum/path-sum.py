1883c7e8a40f23e0292de3faf16e5bc1c9155952
Norbert Shema <shemanorbert11@gmail.com>
2025-10-13 22:38:44 -0400

    Update path-sum.py

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -22,7 +22,7 @@ class Solution(object):
             - If the node is None, return False
             - If it's a leaf and value equals targetSum → return True
             - Recursively check left and right subtrees with updated targetSum
-            - Return True if either path returns True
+            - Return True if either path returns True.
 
         Implement:
         """
