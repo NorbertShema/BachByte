e6f4554d1586c41115cf40b80e4475669a601761
Norbert Shema <shemanorbert11@gmail.com>
2025-11-13 19:18:14 -0500

    Time: 0 ms (100.00%) | Memory: 13.5 MB (30.97%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,17 +5,50 @@ class Solution(object):
         :type target: int
         :rtype: int
 
+        Understand:
+            Input: - List [nums]: an array on integers sorted in asc order
+                   - int[target]: an integer target 
 
+            Output: int[index]: the index of the target in the array[nums]
+
+            Constraints:
+                    -time complexity: o(log n)
+                    -All the integers in nums are unique.
+                    -nums is sorted in ascending order.
+
+            Edge cases:
+                -Empty array
+
+        Match:
+            -Binary search 0(log n )
+
+        Plan:
+            -intilize the points to the first and last index in the array
+        -while left <= right:
+            -find the mid = (left+right)//2
+            - if nums[mid] == target:
+                - return mid
+
+            -if nums[mid] < target:
+                -left = mid +1
+            -else:
+                - right = mid -1 
+
+        return -1 
+
+
+        Implement:
         """
         left = 0
         right =len(nums)-1
+
         while left <= right:
             mid = (left + right )//2
             
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
 
-            elif nums[mid] > target:
+            if nums[mid] > target:
                 right = mid -1
             else:
                 left = mid +1
