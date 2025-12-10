1d1cadd917cb4cd8e4816b4207971909a23888ba
Norbert Shema <shemanorbert11@gmail.com>
2025-12-09 23:17:55 -0500

    Time: 668 ms (14.95%) | Memory: 12.4 MB (80.83%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+
+
+        Understand: Create a F numbers such that each number is the sum of two prev number.
+            Input: int (n)
+
+            Output: the sum at int (n)
+
+            Constraints :
+                -0 <= n <= 30
+
+            Edge cases: These are the base case give when F(0) = 0, F(1) = 1
+
+        Plan: Use recursion to call the stack function for n
+            -Perform the edge cases
+                -F(0) = 0, F(1) = 1
+            - recursion part
+                - F(n>1)
+
+        Implement:  
+
+        """
+        if n== 0:
+            return 0
+        if n==1:
+            return 1 
+        
+        if n>1:
+            return self.fib(n-1) + self.fib(n-2)
+
