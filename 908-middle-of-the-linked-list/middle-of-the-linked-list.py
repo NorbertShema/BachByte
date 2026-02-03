61a2c6d0154ca5bd833e07c879923e48161a0cb3
Norbert Shema <shemanorbert11@gmail.com>
2026-02-03 00:25:06 -0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (23.26%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -3,25 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution(object):
     def middleNode(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-
-        # Initialize two pointers: slow and fast
+        
         slow = head
         fast = head
 
-        # Move fast pointer twice as fast as slow pointer
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # When fast reaches the end, slow is at the middle
-        return slow
+        return slow       
 
-##  Time complexity O(n)
-## Space complexity O(1)
